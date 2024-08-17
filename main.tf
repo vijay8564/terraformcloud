1fca3e55766991952c4e77a2fdbabb91c06ae60b
Vijay Kumar Mekala <158064650+vijay8564@users.noreply.github.com>
2024-08-17 23:12:43 +0530

    Update main.tf
    vnet added

--- main.tf
+++ main.tf
@@ -16,3 +16,10 @@ resource "azurerm_resource_group" "rg1" {
   name     = "southindiaResourceGroup"
   location = "southindia"
 }
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "myTFVnet1"
+  address_space       = ["10.2.0.0/16"]
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+}
